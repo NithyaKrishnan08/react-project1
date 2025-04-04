f4abac2f35d846883159fc1568c2cd74c9e815c6
NithyaKrishnan08 <56358639+NithyaKrishnan08@users.noreply.github.com>
2025-04-04 11:55:47 -0400

    React Routes

---
+++ 07reactRouter/src/components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className="py-16 bg-white">
+      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12"> 
+          <div className="md:7/12 lg:w-6/12">  
+            <p className="mt-4 text-gray-600">
+              Contact us: 
+            </p>
+            <p className="mt-4 text-gray-600">
+              No: 420, Joey's Palace, Atlanta, GA - 30008
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
